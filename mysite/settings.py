54847c4e71840b54eac33759ce3bc0eabdcb9af4
mehrnazd <mehrnazdonyagard@gmail.com>
2021-04-20 15:48:51 +0000

    hey

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,9 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 STATIC_URL = '/static/'
 
